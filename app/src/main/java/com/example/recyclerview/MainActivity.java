da6e3518aa8a8e58f0a66fdebf808fc377ec68b4
Prashant <prasbas97@gmail.com>
2021-03-10 11:45:06 +0545

    Resolved issues-- used h&w in content_main

--- app/src/main/java/com/example/recyclerview/MainActivity.java
+++ app/src/main/java/com/example/recyclerview/MainActivity.java
@@ -23,7 +23,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        for (int i = 0; i < 20; i++) {
+        for (int i = 0; i <50; i++) {
             mWordList.addLast("Word " + i);
         }
 
